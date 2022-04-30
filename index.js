2255e26a379147b4936c9ea116d500710b2f4331
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-04-30 09:42:05 -0400

    creating mobile page

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ function topFunction () {
     document.body.scrollTop = 0; // Safari
     document.documentElement.scrollTop = 0; // Everything else
 }
+
+// For Menu Button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        console.log('We Scrolling now boi!'); //Testing the window.onscroll
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
