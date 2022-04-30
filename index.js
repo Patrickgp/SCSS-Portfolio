c83e3a8c385a5421621135562613c74184f0bf1f
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-04-29 22:53:43 -0400

    Completed transition on home page

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function display() {
-    document.getElementById("sapling").style.filter = "brightness(100%)";
+// For scroll to top button
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction () {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Everything else
 }
